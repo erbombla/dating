10773703f19b4369252ca5e3516380550b4dd1be
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-20 17:54:42 -0700

    tried to fix jQuery. No result

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
   $("form#date").submit(function(event) {
-    var age = parseInt($("input#age").val());
+    var nash = $("#nash").val();
+    var newNash = parseInt(nash);
+
+    if ( newNash === 1 && gender === 'female' ) {
+      $( "#hardy" ).hide();
+      $( "#malek" ).show();
+    }
+    else if ( newNash === 2 && gender === 'female' ) {
+      $( "#malek" ).hide();
+      $( "#hardy" ).show();
+    }
+    if ( newNash === 1 && gender === 'male' ) {
+      $( "#keira" ).hide();
+      $( "#dawson" ).show();
+    }
+    else if ( newNash === 2 && gender === 'male' ) {
+      $( "#dawson" ).hide();
+      $( "#keira" ).show();
+    }
+
     var gender = $("select#gender").val();
+    var age = parseInt($("input#age").val());
 
     if ( gender === 'male' || age < 40 ) {
       $( "#hardy" ).hide();
@@ -9,7 +29,7 @@ $(document).ready(function() {
       $(" #keira" ).hide();
       $(" #dawson" ).show();
     }
-    else ( gender === 'male' || age < 33 ) {
+    else if ( gender === 'male' || age < 33 ) {
       $( "#hardy" ).hide();
       $( "#malek").hide();
       $( "#dawson").hide();
@@ -21,35 +41,13 @@ $(document).ready(function() {
       $(" #keira" ).hide();
       $(" #hardy" ).show();
     }
-    else ( gender === 'female' || age < 38 ) {
+    else if ( gender === 'female' || age < 38 ) {
       $( "#hardy" ).hide();
       $( "#keira").hide();
       $( "#dawson").hide();
       $( "#malek" ).show();
     }
 
-    var nash = $("#nash").val();
-    var newNash = parseInt(nash);
-
-    if ( newNash === 1 && gender === 'female' ) {
-      $( "#hardy" ).hide();
-      $( "#malek" ).show();
-    }
-    else ( newNash === 2 && gender === 'female' ) {
-      $( "#malek" ).hide();
-      $( "#hardy" ).show();
-    }
-    if ( newNash === 1 && gender === 'male' ) {
-      $( "#keira" ).hide();
-      $( "#dawson" ).show();
-    }
-    else ( newNash === 2 && gender === 'male' ) {
-      $( "#dawson" ).hide();
-      $( "#keira" ).show();
-    }
-    else {
-      alert("Please select something");
-    }
     event.preventDefault();
   });
 });
